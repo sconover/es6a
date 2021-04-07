b3c5d0da081f43a50499c686f2382e5dc06de8a8
Steve Conover <steve@chronosphere.io>
2021-04-07 09:30:06 +0200

    test - initial

---
+++ test.js
@@ -0,0 +1,28 @@
+import "https://unpkg.com/mocha@8.3.2/mocha.js"
+import "https://unpkg.com/chai@4.2.0/chai.js"
+import {html, render} from "https://unpkg.com/lit-html@1.2.1?module"
+
+const assert = chai.assert
+
+mocha.setup("tdd")
+
+suite("x", () => {
+    test("foo", () => {
+        assert.equal(1, 2)
+    })
+})
+
+export function renderPage(root) {
+    mocha.checkLeaks()
+    mocha.run()
+}
+
+export function run(root) {
+    render(html`
+        <link href="https://unpkg.com/mocha@8.3.2/mocha.css" rel="stylesheet"/>
+        <div id="mocha"></div>
+    `, root)
+
+    mocha.checkLeaks()
+    mocha.run()
+}
