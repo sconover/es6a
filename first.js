eb08570373f81bbcbf5f8a0412477f84d3002f2f
Steve Conover <sconover@gmail.com>
2021-04-08 12:31:01 +0200

    codesandbox change

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
-import {html, render} from "https://unpkg.com/lit-html@1.2.1?module"
+import { html, render } from "https://unpkg.com/lit-html@1.2.1?module";
 
 export function renderHello(rootE) {
-    render(html`<b>hello</b> world`, rootE)
+  render(html`<b>hello</b> world 2`, rootE);
 }
